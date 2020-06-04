7b45f22ac1194a87749c7efa858168ce30f08036
tom <tom.connolly@protonmail.com>
2020-06-04 21:17:31 +0200

    html dev

--- SplashScreenRoomNet/SplashScreenRoomNet/Index.cpp
+++ SplashScreenRoomNet/SplashScreenRoomNet/Index.cpp
@@ -12,6 +12,8 @@ void Index::HandleRequest(WebServer::http_request* r)
 {
     Socket s = *(r->s_);
     PAGE requestedPage;
+    std::string requestPath = r->path_;
+
     std::map<std::string, std::string> templateValues;
     std::map<PAGE, std::string> pageMappings = { 
         {PAGE::index, "index.html"}, 
@@ -19,20 +21,53 @@ void Index::HandleRequest(WebServer::http_request* r)
     };
     std::vector<std::string> pageSections;
 
-    if (r->path_ == "/") 
+    std::vector<std::string> urlSections = Index::InterpretUrlSections(requestPath);
+
+    if (urlSections.size() > 1)
     {
-        requestedPage = PAGE::index;
-        templateValues["avalue"] = "tom";
+        std::string urlSection1 = urlSections[0];
+
+        if (urlSection1 == "images")
+        {
+            //std::string encoded_png;
+            //Mat img; // Load an image here
+
+            //std::vector<std::uchar> buf;
+            //cv::imencode(".png", img, buf);
+            //auto base64_png = reinterpret_cast<const unsigned char*>(buf.data());
+            //encoded_png = "data:image/jpeg;base64," + std::base64_encode(base64_png, buf.size());
+            r->answer_ = FileInterface::ReadStringFromFile("image.txt");
+            r->content_type_ = "image/jpeg";
+            
+            return;
+        }
+        else if (urlSection1 == "css")
+        {
+
+        }
+        else if (urlSection1 == "js")
+        {
+
+        }
     }
-    else 
-    {
-        requestedPage = PAGE::error;
+    else {
+        if (r->path_ == "/") 
+        {
+            requestedPage = PAGE::index;
+            templateValues["avalue"] = "tom";
+        }
+        else 
+        {
+            requestedPage = PAGE::error;
+        }
     }
 
+
     //add page sections to vector
-    pageSections.push_back(FileInterface::ReadStringFromFile("html/header.html"));
-    pageSections.push_back(FileInterface::ReadStringFromFile(("html/" + pageMappings[requestedPage]).c_str()));
-    pageSections.push_back(FileInterface::ReadStringFromFile("html/footer.html"));
+    std::string htmlDir = "web/html/";
+    pageSections.push_back(FileInterface::ReadStringFromFile((htmlDir + "header.html").c_str()));
+    pageSections.push_back(FileInterface::ReadStringFromFile((htmlDir + pageMappings[requestedPage]).c_str()));
+    pageSections.push_back(FileInterface::ReadStringFromFile((htmlDir + "footer.html").c_str()));
 
     //concatenate page Sections and pass them into the templating engine
     Jinja2CppLight::Template pageTemplate(std::accumulate(pageSections.begin(), pageSections.end(), std::string("")));
@@ -40,6 +75,25 @@ void Index::HandleRequest(WebServer::http_request* r)
     
     //assign page response
     r->answer_ = pageTemplate.render();;
+}
+
+std::vector<std::string> Index::InterpretUrlSections(std::string urlPath)
+{
+    //access sections of request path
+    std::string delimiter = "/";
+    std::vector<std::string> sectionsOfUrl;
+
+    size_t pos = 0;
+    std::string urlSection;
+    while ((pos = urlPath.find(delimiter)) != std::string::npos) {
+        urlSection = urlPath.substr(0, pos);
+        if(urlSection != "") sectionsOfUrl.push_back(urlSection);
+        urlPath.erase(0, pos + delimiter.length());
+    }
+
+    if (urlSection != "") sectionsOfUrl.push_back(urlPath);
+
+    return sectionsOfUrl;
 }
 
 void Index::InsertTemplateValues(Jinja2CppLight::Template* jinjaTemplate, std::map<std::string, std::string> values)
--- SplashScreenRoomNet/SplashScreenRoomNet/Index.h
+++ SplashScreenRoomNet/SplashScreenRoomNet/Index.h
@@ -12,4 +12,5 @@ private:
 		index,
 		error
 	};
+	static std::vector<std::string> InterpretUrlSections(std::string urlPath);
 };
