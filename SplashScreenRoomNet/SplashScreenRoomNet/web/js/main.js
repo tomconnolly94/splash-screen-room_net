a085b0ce1c00afd71cb2c82bd3f76334222756e3
tom <tom.connolly@protonmail.com>
2020-07-08 23:08:20 +0200

    new image files and web dev start

--- SplashScreenRoomNet/SplashScreenRoomNet/web/js/main.js
+++ SplashScreenRoomNet/SplashScreenRoomNet/web/js/main.js
@@ -1,1 +1,23 @@
-alert(" it works boi!");
+//alert(" it works boi!");
+
+// delegate event for performance, and save attaching a million events to each anchor
+
+var link_tags = document.getElementsByClassName("resource-link");
+
+for (var i = 0; i < link_tags.length; i++) {
+
+    link_tags[i].addEventListener('click', function (event) {
+
+        event.preventDefault();
+
+        var target = this;
+        if (target.tagName.toLowerCase() == 'a') {
+            var port = target.getAttribute('href').match(/^(.*):(\d+)(.*)/);
+            if (port) {
+                window.location.port = port[2];
+                window.location = port[1];
+            }
+        }
+    }, false);
+
+} 
