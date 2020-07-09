fd8903f70ba57b6bfbd509714a43f7c5c5b07f54
tom <tom.connolly@protonmail.com>
2020-07-09 19:54:17 +0200

    sync changes

--- SplashScreenRoomNet/SplashScreenRoomNet/web/js/main.js
+++ SplashScreenRoomNet/SplashScreenRoomNet/web/js/main.js
@@ -1,1 +1,14 @@
-alert(" it works boi!");
+//alert(" it works boi!");
+
+// delegate event for performance, and save attaching a million events to each anchor
+
+var link_tags = document.getElementsByClassName("resource-link");
+
+for (var i = 0; i < link_tags.length; i++) {
+
+    link_tags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        window.open(this.getAttribute('href'), "_blank");
+    }, false);
+
+} 
